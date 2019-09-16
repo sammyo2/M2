e2d98548aba9482a7530b46fdb61a061082767b8
srobinson75 <srobinson75@gatech.edu>
2019-09-16 18:14:06 -0400

    completed Person1

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,11 +3,22 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
+ *  modified string
+ *
+ *  @author Shelby
  *  @version 1.1
  */
+
+
+public static void main (String[] args) {
+  Person p1 = new Person("Shelby");
+
+  String result = p1.calc("gtg123b");
+
+  System.out.println(result);
+
+}
+
 public class Person1 {
   /** Holds the persons real name */
   private String name;
@@ -16,6 +27,7 @@ public class Person1 {
 	 * name
 	 * @param pname the person's real name
 	 */
+
   public Person1(String pname) {
     name = pname;
   }
@@ -31,15 +43,23 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+
+    for(int i  = 0; i < 2; i++) {
+      for(int j = 1; j < input.length(); j++) {
+        char temp = input.charAt(0);
+        inut.charAt(j - 1) = input.charAt(j);
+      }
+      input.charAt(input.charAt(input.length() -1 )) = temp;
+    }
+    return input;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
